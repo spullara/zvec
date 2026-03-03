4f5251bcdfaf5a6ce025d1c5d5fa4f2f8102d3fc
Sam Pullara <spullara@gmail.com>
2026-03-03 11:41:27 -0800

    build: use local xcframework instead of remote binary
    Agent-Id: agent-9ffda7fc-3092-4e98-8c9e-eff10cd9324e

--- Package.swift
+++ Package.swift
@@ -10,8 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "zvec",
-            url: "https://github.com/spullara/zvec/releases/download/v0.1.0-ios/zvec.xcframework.zip",
-            checksum: "2934d9e5aa5f9c9abab46897685922ca75cb990ddbc21c97965ebebe5e2b9305"
+            path: "build-xcframework/zvec.xcframework"
         ),
         .target(
             name: "CZvec",
